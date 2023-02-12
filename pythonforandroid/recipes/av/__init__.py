26ba6dda37b83c8ab9f77847479fa049255e3ffd
Dexer <73297572+DexerBR@users.noreply.github.com>
2023-02-12 13:53:57 -0300

    Update __init__.py

--- pythonforandroid/recipes/av/__init__.py
+++ pythonforandroid/recipes/av/__init__.py
@@ -1,9 +1,10 @@
 from pythonforandroid.toolchain import Recipe
 from pythonforandroid.recipe import CythonRecipe
-from pythonforandroid.toolchain import Recipe
 
 
 class PyAV(CythonRecipe):
+
+    name = 'av'
     version = 'v10.0.0'
     url = 'https://github.com/PyAV-Org/PyAV/archive/v10.0.0.zip'
 
@@ -14,8 +15,8 @@ class PyAV(CythonRecipe):
         env = super().get_recipe_env(arch)
 
         build_dir = Recipe.get_recipe('ffmpeg', self.ctx).get_build_dir(arch.arch)
-        env["PYAV_TESTDATA_DIR"] = self.get_build_dir(arch.arch)
         self.setup_extra_args = ["--ffmpeg-dir={}".format(build_dir)]
+        env["PYAV_TESTDATA_DIR"] = self.get_build_dir(arch.arch)
 
         return env
 
